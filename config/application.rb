0b4c2c91b0cb5e8a984473f42ccf1c3f7f1b224c
HT Human Tecnologies <46276431+humantechnologies@users.noreply.github.com>
2019-02-03 15:07:20 +0100

    Add files via upload
    Configure I18n localization. aggiunta lingua it (italiano)

--- config/application.rb
+++ config/application.rb
@@ -34,11 +34,12 @@ module Greenlight
     config.exceptions_app = routes
 
     # Configure I18n localization.
+    # GT20190113 aggiunta it nelle available.locales
     config.i18n.available_locales = %w(en pt-br es ar fr de el it ru)
-    config.i18n.default_locale = "it"
+    config.i18n.default_locale = "en"
 
     config.i18n.available_locales.each do |locale|
-      config.i18n.fallbacks[locale] = [locale, :it]
+      config.i18n.fallbacks[locale] = [locale, :en]
     end
 
     # Check if a loadbalancer is configured.
