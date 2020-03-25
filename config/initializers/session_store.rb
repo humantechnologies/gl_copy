0a05218ad6cc0d8a90781e32c20100f3b11d438b
HT Human Tecnologies <46276431+humantechnologies@users.noreply.github.com>
2020-03-25 07:45:17 +0100

    riunioni.online

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,5 @@
 # Be sure to restart your server when you modify this file.
 
 # GT20190119 Rails.application.config.session_store :cookie_store, key: '_greenlight-2_0_session'
-Rails.application.config.session_store :cookie_store, key: 'chiamami_online_session', expire_after: 30.days
+Rails.application.config.session_store :cookie_store, key: 'Riunioni_online_session', expire_after: 30.days
 
